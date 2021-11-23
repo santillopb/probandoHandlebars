94e3ba46f3f4326b8e3fdb51ef368baf82475c26
Jano Soler <jano.soler@xarxatecactiva.com>
2021-11-23 23:31:00 +0100

    feat: static y form con urlencoded

--- app.ts
+++ app.ts
@@ -1,23 +1,25 @@
 import express from 'express';
 
 const expressHandlebars = require('express-handlebars');
-
 const hbs = expressHandlebars.create({defaultLayout: "main"});
 
-const app:express.Application = express();
+const path = require('path');
 
+const app:express.Application = express();
 
 app.engine('handlebars', hbs.engine);
 app.set('view cache', true);
 app.set('view engine', 'handlebars');
 
-app.get('/', (req:express.Request, res:express.Response):void=>{
-    res.render("helloWorld.handlebars", {message: "Mundo!"});
-});
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/saluda', (req:express.Request, res:express.Response):void=>{
-    res.render("saluda.handlebars",{datos: [{nombre: "Jano", apellido: "Soler"},{nombre: "Pilar", apellido: "Espinosa"}, {nombre: "Aarón", apellido: "Bellés"}]});
-});
+app.use(express.static(path.join( __dirname, '..', 'public')));
+
+
+app.post("/alta", (req: express.Request, res:express.Response):void=>{
+    res.render("saluda.handlebars", {datos:{nombre: req.body.nombre, apellido: req.body.apellido}})
+})
+
 
 app.listen(3000, ():void=>{
     console.log('App escuchando el puerto 3000...')
--- build/app.js
+++ build/app.js
@@ -6,18 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const expressHandlebars = require('express-handlebars');
 const hbs = expressHandlebars.create({ defaultLayout: "main" });
+const path = require('path');
 const app = (0, express_1.default)();
-//const expressHandlebars: ExpressHandlebars = create({defaultLayout: "main", extname: ".hbs"});
-//console.log(expressHandlebars);
-//app.engine('handlebars', require('express-handlebars').create({defaultLayout: "main"}).engine);
 app.engine('handlebars', hbs.engine);
 app.set('view cache', true);
 app.set('view engine', 'handlebars');
-app.get('/', (req, res) => {
-    res.render("helloWorld.handlebars", { message: "Mundo!" });
-});
-app.get('/saluda', (req, res) => {
-    res.render("saluda.handlebars", { datos: [{ nombre: "Jano", apellido: "Soler" }, { nombre: "Pilar", apellido: "Espinosa" }, { nombre: "Aarón", apellido: "Bellés" }] });
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.static(path.join(__dirname, '..', 'public')));
+app.post("/alta", (req, res) => {
+    res.render("saluda.handlebars", { datos: { nombre: req.body.nombre, apellido: req.body.apellido } });
 });
 app.listen(3000, () => {
     console.log('App escuchando el puerto 3000...');
